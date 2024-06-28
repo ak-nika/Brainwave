452fb3d04dc12a7d81ff7b0cf8121be571561b54
ak-nika <akingbayiojo@gmail.com>
2024-06-28 16:26:11 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./sections/Benefits";
+import Collaboration from "./sections/Collaboration";
+import Footer from "./sections/Footer";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Pricing from "./sections/Pricing";
+import Roadmap from "./sections/Roadmap";
+import Services from "./sections/Services";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Tagline.jsx
@@ -0,0 +1,14 @@
+import bracket from "../assets/svg/Bracket";
+
+
+const Tagline = ({ className, children }) => {
+  return (
+    <div className={`tagline flex items-center ${className || ""}`}>
+      {bracket('left')}
+      <div className="mx-3 text-n-3">{children}</div>
+      {bracket('right')}
+    </div>
+  );
+};
+
+export default Tagline;
